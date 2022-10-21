a376383b673776b06f96de5aba98a892bc5bc54e
itsnuel <nuelmutwiri@gmail.com>
2022-10-21 21:57:53 +0300

    airplane

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,94 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
+import 'package:tickets/utils/app_styles.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: styles.bgcolor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good morning",
+                          style: styles.headLineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          "Book Tickets",
+                          style: styles.headLineStyle1,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/img_1.png"))),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6FD),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 12),
+                    child: Row(
+                      children: [
+                        const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                            color: Color(0xFFBFC205)),
+                        Text(
+                          "Search",
+                          style: styles.headLineStyle4,
+                        )
+                      ],
+                    )),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Upcoming Flights",
+                      style: styles.headLineStyle2,
+                    ),
+                    InkWell(
+                        onTap: () {
+                          print("You are tapped");
+                        },
+                        child: Text(
+                          "View all",
+                          style: styles.textStyle
+                              .copyWith(color: styles.primarycolor),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/ticket_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:tickets/utils/app_layout.dart';
+
+class TicketView extends StatelessWidget {
+  const TicketView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getsize(context); 
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+}
